6796086fc4e2b6faac20e047d9758566e29d28dc
'CHARLIE <'CHARLES.SHORB@COLORADOCOLLEGE.EDU'>
2017-05-09 10:26:22 -0600

    COOL FOOD IS BAD

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -3,5 +3,6 @@ public class OurFavorites {
 	public static void main(String[] args){
 		String favorites = "COOL FOOD";
 		System.out.println("My favorite is... " + favorites + "!");
+		System.out.println("...But this is actually not my favorite. :(");
 	}
 }

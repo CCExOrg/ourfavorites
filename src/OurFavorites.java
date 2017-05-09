6fb3b139629a04d36a690c0a1c5d11aae2bfd7b8
'CHARLIE <'CHARLES.SHORB@COLORADOCOLLEGE.EDU'>
2017-05-09 10:42:07 -0600

    Reordered string print, now makes a tiny bit more sense from none.

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -1,9 +1,9 @@
 
 public class OurFavorites {
 	public static void main(String[] args){
-		Snack snack = new Snack("APPL");
+		Snack snack = new Snack("APPLE");
 		System.out.println("My favorite is... " + snack.getName() + "!");
-		System.out.println("haha no but for real, what's going on");
 		System.out.println("...But this is actually not my favorite. :(");
+		System.out.println("haha no but for real, what's going on");
 	}
 }

89f2b5cd94d9ef201191595d14901e374bac7192
'CHARLIE <'CHARLES.SHORB@COLORADOCOLLEGE.EDU'>
2017-05-09 10:13:54 -0600

    NECESSARY COMMENTS

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -1,6 +1,7 @@
 
 public class OurFavorites {
 	public static void main(String[] args){
+		// CRANBERRIES YO
 		String favorites = "CRANBERRIES";
 		System.out.println("My favorite is... " + favorites + "!");
 	}

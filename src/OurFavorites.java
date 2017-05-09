52d561bf1ac73aa75d3e18453d2727616f607bb6
zack <zack@172.16.71.48>
2017-05-09 10:23:38 -0600

    New branch with TWO (2) print messages   !!!!!!

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -3,5 +3,6 @@ public class OurFavorites {
 	public static void main(String[] args){
 		String favorites = "COOL FOOD";
 		System.out.println("My favorite is... " + favorites + "!");
+		System.out.println("haha no but for real, what's going on");
 	}
 }

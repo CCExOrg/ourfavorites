63f9103ff0a2630d886dcf3881b0062852af70ef
zack <zack@172.16.71.48>
2017-05-09 10:15:07 -0600

    CHANGED TO NAVORITES, FAVORITES NOT ENOUGH N

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -1,7 +1,7 @@
 
 public class OurFavorites {
 	public static void main(String[] args){
-		String favorites = "APPLES";
-		System.out.println("My favorite is... " + favorites + "!");
+		String navorites = "COOL FOOD";
+		System.out.println("My favorite is... " + navorites + "!");
 	}
 }

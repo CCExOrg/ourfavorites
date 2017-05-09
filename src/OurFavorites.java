468aeaf229dbcc49a35ea7dcda060b63ca099c42
'CHARLIE <'CHARLES.SHORB@COLORADOCOLLEGE.EDU'>
2017-05-09 10:10:52 -0600

    PLURAL AGAIN

--- src/OurFavorites.java
+++ src/OurFavorites.java
@@ -1,7 +1,7 @@
 
 public class OurFavorites {
 	public static void main(String[] args){
-		String favorites = "CRANBERRY SINGULAR";
+		String favorites = "CRANBERRIES";
 		System.out.println("My favorite is... " + favorites + "!");
 	}
 }
